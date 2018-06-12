d0e0186c0aacb8bb080d1facf2b89727f2da4933
matheusces <40028977+matheusces@users.noreply.github.com>
2018-06-12 12:06:01 -0300

    Arquivos do trabalho de android

---
+++ SplashActivity.java
@@ -0,0 +1,28 @@
+package br.com.minhaempresa.funapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mMainActivity();
+            }
+        }, 2000);
+    }
+
+    private void mMainActivity() {
+        Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
